f17b4459ca556657f3ebcdacb1108a82a167db68
Nishu Goel <n.goel@epilot.cloud>
2022-03-29 00:27:06 +0530

    init

---
+++ src/i18n-store.ts
@@ -0,0 +1,31 @@
+import type { i18n } from "i18next";
+import { writable, type Readable, type Writable } from "svelte/store";
+
+
+export interface TranslationService {
+    i18n: Readable<i18n>;
+}
+
+export class I18NextTranslationStore implements TranslationService {
+    public i18n: Writable<i18n>;
+
+    constructor(i18n: i18n, ns: string | string[]) {
+        this.i18n = this.createInstance(i18n, ns);
+    }
+
+    private createInstance(i18n: i18n, ns: string | string[]): Writable<i18n> {
+        const i18n_writable = writable(i18n)
+
+        i18n.on('initialized', () => {
+            i18n.loadNamespaces(ns)
+            i18n_writable.set(i18n)
+        })
+        i18n.on('onLoaded', () => i18n_writable.set(i18n))
+        i18n.on('added', () => i18n_writable.set(i18n))
+        i18n.on('languageChanged', () => {
+            i18n.loadNamespaces(ns);
+            i18n_writable.set(i18n)
+        })
+        return i18n_writable;
+    }
+}
