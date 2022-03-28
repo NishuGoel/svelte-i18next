32e6a6b0ea0507e9cf70f32b60679049efa3d698
Nishu Goel <nishu0505@gmail.com>
2022-03-29 00:36:02 +0530

    Create i18next wrapper for svelte

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import type { i18n } from 'i18next';
+import { I18NextTranslationStore } from './i18n-store';
+
+
+export const createI18nStore = (i18n: i18n, ns: string | string[]) => {
+    const i18nStore = new I18NextTranslationStore(i18n, ns);
+    return i18nStore.i18n;
+}
