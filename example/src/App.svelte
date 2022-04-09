ffdc469f5059a343e78a0b6f657eee5538f3e50b
Nishu Goel <nishu0505@gmail.com>
2022-04-10 00:50:46 +0530

    Add isLoading flag for a fallback (#3)
    * add isLoading flag
    
    * update svelte-i18next example project with isLoading flag
    
    * update svelte-i18next example project with isLoading flag
    
    * update isLoading implementation
    
    Co-authored-by: Nishu Goel <n.goel@epilot.cloud>

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -2,7 +2,7 @@
   import i18next from "i18next";
   import HttpBackend from "i18next-browser-languagedetector";
   import LanguageDetector from "i18next-http-backend";
-  import { createI18nStore } from "svelte-i18next";
+  import { createI18nStore, isLoading } from "svelte-i18next";
 
   i18next
     .use(HttpBackend)
@@ -23,9 +23,12 @@
   const i18n = createI18nStore(i18next);
 </script>
 
-<section
-  on:click={() => $i18n.changeLanguage($i18n.language === "en" ? "de" : "en")}
->
-  {$i18n.t("Hello")}
-  {$i18n.t("World")}
-</section>
+{#if $isLoading}
+  <p>loading...</p>
+{:else}
+  <section
+    on:click={() => $i18n.changeLanguage($i18n.language === "en" ? "de" : "en")}>
+    {$i18n.t("Hello")}
+    {$i18n.t("World")}
+  </section>
+{/if}
--- src/translation-store.ts
+++ src/translation-store.ts
@@ -6,24 +6,45 @@ export interface TranslationService {
     i18n: Readable<i18n>;
 }
 
+export const isLoading = writable(true);
+
 export class I18NextTranslationStore implements TranslationService {
     public i18n: Writable<i18n>;
+    public isLoading: Writable<boolean>;
 
     constructor(i18n: i18n) {
         this.i18n = this.createInstance(i18n);
+        this.isLoading = this.createLoadingInstance(i18n);
     }
 
     private createInstance(i18n: i18n): Writable<i18n> {
-        const i18n_writable = writable(i18n)
+        const i18nWritable = writable(i18n)
 
         i18n.on('initialized', () => {
-            i18n_writable.set(i18n)
+            i18nWritable.set(i18n)
         })
-        i18n.on('onLoaded', () => i18n_writable.set(i18n))
-        i18n.on('added', () => i18n_writable.set(i18n))
+        i18n.on('loaded', () => {
+            i18nWritable.set(i18n)
+        })
+        i18n.on('added', () => i18nWritable.set(i18n))
         i18n.on('languageChanged', () => {
-            i18n_writable.set(i18n)
+            i18nWritable.set(i18n)
         })
-        return i18n_writable;
+        return i18nWritable;
+    }
+
+    private createLoadingInstance(i18n: i18n): Writable<boolean> {
+        // if loaded resources are empty || {}, set loading to true
+        i18n.on('loaded', (resources) => {
+            Object.keys(resources).length !== 0 && isLoading.set(false)
+        })
+
+        // if resources failed loading, set loading to true
+        i18n.on('failedLoading', () => {
+            isLoading.set(true)
+        })
+
+        return isLoading;
     }
 }
+
